79d69caa68d3f5c2cbff792781028c89a65d6a1b
rewpt <zpermack@gmail.com>
2021-06-18 18:06:26 -0400

    Created initial react program for Robofriends, with Hello and red BG

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css';
+class Hello extends Component {
+  render() {
+    return(
+    <div>
+    <h1> Hello World </h1>
+    <h2> Another title</h2>
+    </div>
+    )
+  }
+
+}
+
+export default Hello;
