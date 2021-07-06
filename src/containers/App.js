cc0d1ccc0bf088f8f4d2e3d4c3e29626172140c8
rewpt <zpermack@gmail.com>
2021-07-06 17:50:42 -0400

    Finished robofriends app with refactored code

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
+  render() {
+    //destructuring so that we can use robots and searchField without this.state every time
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+    //ternary statement with return before the statement
+    return !robots.length ? <h1>Loading</h1> :
+      (
+        <div className='tc' >
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+  }
+}
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,53 @@
+export const robots = [
+  {
+    id: '1',
+    name: 'Jerry Seinfeld',
+    email: 'jerryseinfeld@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Cosmo Cramer',
+    email: 'cosmo@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Elaine Bennis',
+    email: 'elaineb@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Newman',
+    email: 'newman55@gmail.com',
+  },
+  {
+    id: 5,
+    name: 'George Costanza',
+    email: 'georgec@gmail.com',
+  },
+  {
+    id: 6,
+    name: 'Bart Simpsons',
+    email: 'barts@gmail.com',
+  },
+  {
+    id: 7,
+    name: 'Lisa Simpson',
+    email: 'lisas@gmail.com',
+  },
+  {
+    id: 8,
+    name: 'Maggie Simpson',
+    email: 'maggies@gmail.com',
+  },
+  {
+    id: 9,
+    name: 'Marge Simpson',
+    email: 'simpsonmd@gmail.com',
+  },
+  {
+    id: 10,
+    name: 'Homer Simpsons',
+    email: 'homers@gmail.com',
+  }
+
+]
