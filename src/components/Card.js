26287e802d4d7478c758463a23d6a62b3149097d
rewpt <zpermack@gmail.com>
2021-07-07 13:13:15 -0400

    another try

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Card = ({name, email, id}) => {
+const Card = ({ name, email, id }) => {
   return (
     //Using tachyon classes to style the cards
-    <div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src = {`https://robohash.org/${id}?200x200`} alt = 'robot' />
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img src={`https://robohash.org/${id}?200x200`} alt='robot' />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
