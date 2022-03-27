c29e7795a38bd9fd68f6d31776b15ee5681bedbe
rewpt <zpermack@gmail.com>
2022-03-26 20:47:45 -0400

    add switch statement for seachrobots

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,5 @@
-import { CHANGE_SEARCH_FIELD} from './constants.js';
+import {CHANGE_SEARCH_FIELD, REQUEST_ROBOTS_FAILED, REQUEST_ROBOTS_SUCCESS, REQUEST_ROBOTS_PENDING
+} from './constants'
 
 const initialState = {
   searchField: ''
@@ -8,6 +9,22 @@ export const searchRobots = (state=initialState, action={}) => {
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, {searchField : action.payload} )
+    default:
+      return state;
+  }
+}
+
+export const requestRobots = (state=initialState, action={}) => {
+  switch(action.type) {
+    case REQUEST_ROBOTS_PENDING:
+    return Object.assign({}, state, {isPending: true})
+    
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, {robots: action.payload, isPending:false})
+
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, {error: action.payload, isPending: false})
+    
     default:
       return state;
   }
